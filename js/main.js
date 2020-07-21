9fd493305f443bb25349bc2f51be3808806762b7
DenisAleshkov <aleshkov592@gmail.com>
2020-07-21 19:27:24 +0300

    add working app

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
 const api = {
 	key: 'ebc1a7836b5af40d808a2ffcb4bd00d2',
-	// url: 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}',
 	url: 'https://api.openweathermap.org/data/2.5/'
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    	fetch(`${api.url}weather?q=Mogilev&appid=${api.key}`)
-			.then(res => res.json())
-			.then(res=>displayWeather(res.name, res.main.temp,  res.weather[0].icon));	
+document.addEventListener('DOMContentLoaded', async () => {
+	try{
+		let weatherData = await (await fetch(`${api.url}weather?q=Mogilev&appid=${api.key}`)).json()
+		console.log(weatherData)
+		displayWeather(weatherData)
+		setTimeout(()=>document.querySelector('.current').style.opacity = 1, 100)
+	}catch(e){}
 })
 
-  document.querySelector('.search-city').addEventListener('keydown', function(e) {
-    	if (e.keyCode === 13) {
-    		
-    			fetch(`${api.url}weather?q=${this.value}&appid=${api.key}`)
-				.then(res => res.json())
-				.then(res=>{
-					displayWeather(res.name, res.main.temp,  res.weather[0].icon)
-				});
-
+   document.querySelector('.search-city').addEventListener('keydown', async function(e) {
+		if (e.keyCode === 13) {
+    		try{
+				let weatherData = await (await fetch(`${api.url}weather?q=${this.value}&appid=${api.key}`)).json()
+				displayWeather(weatherData)
 				this.value = ''
-    		
+			}catch(e){}
   		}	
 	})
+function displayWeather(weather) {
 
-function displayWeather(resCity, temp, weatherIcon) {
-	let icon = document.querySelector('.icon');
-	icon.className = 'icon-'+ weatherIcon;
-	// icon.classList.toggle('icon-'+ weatherIcon);
-	// icon.classList.add('icon-'+ weatherIcon);
+	let currentBlock = document.querySelector('.current');
+	let icon = currentBlock.childNodes[7]
+	icon.className = 'icon-' + weather.weather[0].icon;
+
 	const gradus = document.querySelector('.current-gradus');
-	gradus.textContent = Math.round(temp - 273);
-	const city = document.querySelector('.current-city')
-	city.textContent = resCity
+	gradus.textContent = Math.round(weather.main.temp - 273);
+
+	const city = document.querySelector('.city-name')
+	city.textContent = weather.name
+
+	const citySymbols = document.querySelector('.city-symbols')
+	citySymbols.textContent = weather.sys.country
 }
